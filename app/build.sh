10e38bd45c08ed1e8890c06dc855617211ce5253
ADZIRI <adamecell@gmail.com>
2026-07-28 12:56:39 +0200

    fix: force Python 3.12 in build commands

--- app/build.sh
+++ app/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Installer les dépendances avec uv (gestionnaire de Vercel)
-uv pip install --system -r requirements.txt
+# Installer les dépendances avec Python 3.12
+uv pip install --system --python 3.12 -r requirements.txt
 
 # Collecter les fichiers statiques
-python manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput
 
 # Appliquer les migrations
-python manage.py migrate --noinput
+python3.12 manage.py migrate --noinput
