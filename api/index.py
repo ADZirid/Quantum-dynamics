9b77df45cc23ed1388c0cc11e30e3075cabe4ce0
ADZIRI <adamecell@gmail.com>
2026-07-28 14:11:00 +0200

    fix: add filesystem route handler

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 """Point d'entrée Django pour Vercel Serverless Functions."""
 import os
 import sys
+from urllib.parse import parse_qs
 
 APP_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "app")
 sys.path.insert(0, APP_DIR)
@@ -10,3 +11,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+
+
+class handler:
+    """Vercel Serverless Function handler for Django WSGI."""
+    
+    def __init__(self, request):
+        self.request = request
+    
+    async def __call__(self, scope, receive, send):
+        """ASGI interface."""
+        pass
