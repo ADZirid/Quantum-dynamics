9e345b350433d4ed78fc4c8210c475b7c50f2520
ADZIRI <adamecell@gmail.com>
2026-07-28 12:57:56 +0200

    fix: downgrade to Django 4.2 LTS for Python 3.9 compatibility

--- app/build.sh
+++ app/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Installer les dépendances avec Python 3.12
-uv pip install --system --python 3.12 -r requirements.txt
+# Installer les dépendances
+uv pip install --system -r requirements.txt
 
 # Collecter les fichiers statiques
-python3.12 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Appliquer les migrations
-python3.12 manage.py migrate --noinput
+python manage.py migrate --noinput
