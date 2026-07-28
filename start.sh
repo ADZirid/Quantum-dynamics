a1d98ab5ab94c5e5dca325467970deee6fda29fb
ADZIRI <adamecell@gmail.com>
2026-07-28 20:49:22 +0200

    fix: Dockerfile at root - Render will use Docker instead of buildpack

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-echo "=== Running migrate ==="
-python app/manage.py migrate --noinput
-echo "=== Migrate done ==="
-
-echo "=== Starting gunicorn ==="
-exec gunicorn config.wsgi:application -b 0.0.0.0:$PORT --pythonpath app --workers 2 --timeout 60
