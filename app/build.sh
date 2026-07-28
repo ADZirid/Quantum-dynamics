ac75f0b281edd504e6d14d335c8c52fe5b3213e3
ADZIRI <adamecell@gmail.com>
2026-07-28 13:52:59 +0200

    fix: use uv run for Django commands in build

--- app/build.sh
+++ app/build.sh
@@ -4,10 +4,10 @@ set -e
 cd app
 
 # Installer les dépendances
-uv pip install --system -r requirements.txt
+uv pip install -r requirements.txt
 
 # Collecter les fichiers statiques
-python manage.py collectstatic --noinput
+uv run python manage.py collectstatic --noinput
 
 # Appliquer les migrations
-python manage.py migrate --noinput
+uv run python manage.py migrate --noinput
